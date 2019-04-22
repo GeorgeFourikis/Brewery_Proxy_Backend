bfd4351269b8e0c23e7afcad6d0176a3d21204f2
vlahunter <vlahunter@hotmail.com>
2019-04-22 08:01:37 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const axios = require("axios");
+const app = express();
+const port = process.env.PORT || 4000;
+
+dotenv.config();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log(process.env.BREWERY_API_KEY);
+  res.send("Hello World");
+});
+
+app.get("/beers", async (req, res) => {
+  if (req.query) {
+    page = req.query.page;
+    console.log(page);
+  }
+  let beers;
+  let processed;
+  try {
+    beers = await axios.get(
+      `http://api.brewerydb.com/v2/beers/?key=${
+        process.env.BREWERY_API_KEY
+      }&p=${page}`
+    );
+    processed = beers.data.data;
+  } catch (e) {
+    console.log(e);
+    return "ERROR";
+  }
+  const finalData = {
+    info: {
+      currentPage: beers.data.currentPage,
+      numberOfPages: beers.data.numberOfPages,
+      totalResults: beers.data.totalResults
+    },
+    data: processed
+  };
+  console.log("OK...");
+  res.send(finalData);
+});
+
+// app.get("/beer/:id", async (req, res) => {
+//   const id = req.params.id;
+//   try {
+//     beer = await axios.get(
+//       `http://api.brewerydb.com/v2/beer/${id}/?key=${
+//         process.env.BREWERY_API_KEY
+//       }`
+//     );
+//   } catch (e) {
+//     console.log(e);
+//   }
+//   console.log("Done");
+//   res.send(beer.data);
+// });
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Listening on port ${port}...`);
+});
